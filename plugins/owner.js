6762fd751941e00b6081e4936151395e2a268fa2
malikahmu <167111962+malikahmu@users.noreply.github.com>
2025-01-04 13:01:26 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,8 +11,8 @@ cmd({
 async (conn, mek, m, { from }) => {
     try {
         // Owner's contact info
-        const ownerNumber = '+447783770746'; // Replace this with the actual owner number
-        const ownerName = 'SOBIA BUTT'; // Replace this with the owner's name
+        const ownerNumber = '+923338541975'; // Replace this with the actual owner number
+        const ownerName = 'MALIK AHMED'; // Replace this with the owner's name
         const organization = 'UD TEAM'; // Optional: replace with the owner's organization
 
         // Create a vCard (contact card) for the owner
